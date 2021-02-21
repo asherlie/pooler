8efd1659ad63c532bc381056caae74a57035783f
asherlie <lieber.asher@gmail.com>
2021-02-20 22:44:51 -0500

    spawn_thread() now takes in bool* that is occupied with success of thread creation, create_tll() now takes a struct thread*

--- pool.c
+++ pool.c
@@ -11,17 +11,27 @@ struct func_arg{
     volatile _Bool exit;
 };
 
+struct thread{
+    struct func_arg* f_a;
+    pthread_t pth;
+};
+
+
 /* linked list containing threads
  */
 struct thread_ll{
-    pthread_t thread;
-    struct func_arg* f_a;
+    struct thread* thread_info;
+    /*
+     * pthread_t thread;
+     * struct func_arg* f_a;
+     */
     struct thread_ll* next;
 };
 
-struct thread_ll* create_tll(pthread_t pth){
+struct thread_ll* create_tll(struct thread* th){
     struct thread_ll* ret = malloc(sizeof(struct thread_ll));
-    ret->thread = pth;
+    ret->thread_info = th;
+    /* ret->thread = pth; */
     return ret;
 }
 
@@ -57,23 +67,19 @@ void* await_instructions(void* v_f_a){
     return NULL;
 }
 
-struct thread{
-    struct func_arg* f_a;
-    pthread_t pth;
-};
-
-struct thread* spawn_thread(){
+struct thread* spawn_thread(_Bool* success){
     struct thread* ret = malloc(sizeof(struct thread));
     ret->f_a = malloc(sizeof(struct func_arg));
     ret->f_a->exit = 0;
     ret->f_a->spool_up = 0;
     /*ret->f_a->func =*/ ret->f_a->arg = NULL;
 
+    #pragma GCC diagnostic ignored "-Wuninitialized"
     pthread_t pth;
 
     ret->pth = pth;
-
-    pthread_create(&ret->pth, NULL, await_instructions, ret->f_a);
+    _Bool failure = pthread_create(&ret->pth, NULL, await_instructions, ret->f_a);
+    if(success)*success = !failure;
 
     return ret;
 }
@@ -81,15 +87,18 @@ struct thread* spawn_thread(){
 void init_pool(struct thread_pool* p, int n_threads){
     p->n_threads = n_threads;
     /* p->available = insert_tll(&); */
+    struct thread* th;
     for(int i = 0; i < p->n_threads; ++i){
-        pthread_t pth;
         /* pthread_attr_init(&pth); */
-        insert_tll(&p->available, create_tll(pth));
+        th = spawn_thread(NULL);
+
+        insert_tll(&p->available, create_tll(th));
     }
     p->in_use = NULL;
 }
 
 void spool_up(struct thread_pool* p){
+    (void)p;
 }
 
 /*
